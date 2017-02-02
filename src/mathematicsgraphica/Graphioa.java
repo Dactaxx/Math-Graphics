36b135454c7fa4fba129d74e2ff6c07bb126e181
Dillan Pribak <dmpribak@gmail.com>
2017-02-01 20:06:37 -0500

    Initial commit

---
+++ src/mathematicsgraphica/Graphioa.java
@@ -0,0 +1,5 @@
+package mathematicsgraphica;
+
+public class Graphioa {
+
+}
---
+++ src/mathematicsgraphica/Window.java
@@ -0,0 +1,10 @@
+package mathematicsgraphica;
+
+public class Window {
+	public static double WIDTH, HEIGHT;
+	public static void createWindow(double width, double height) {
+		WIDTH = width;
+		HEIGHT = height;
+		
+	}
+}
